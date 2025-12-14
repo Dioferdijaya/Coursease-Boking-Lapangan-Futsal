c63d833fc2522e47a4600728863f11aa948eb246
Randy Maulana <randy8@mhs.usk.ac.id>
2025-12-14 07:46:25 +0700

    refactor: change API_URL import path from api to config

--- booking-frontend/src/pages/AdminBookings.js
+++ booking-frontend/src/pages/AdminBookings.js
@@ -2,7 +2,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import API_URL from "../api";
+import API_URL from "../config";
+
 
 import "./AdminBookings.css";
 

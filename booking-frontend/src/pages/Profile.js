b2ac2e768e8c93caa199f55f0dca2c2a606e8001
Randy Maulana <randy8@mhs.usk.ac.id>
2025-12-14 07:52:59 +0700

    refactor: change API_URL import path from api to config

--- booking-frontend/src/pages/Profile.js
+++ booking-frontend/src/pages/Profile.js
@@ -2,7 +2,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
-import API_URL from "../api";
+import API_URL from "../config";
+
 
 import ChatBox from "./ChatBox";
 import "./Profile.css";
